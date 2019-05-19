196a05eafd4435dc7ce36ae7a94736e67f8f778e
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-05-19 20:18:13 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,8 @@ cp entropy /bin
 cp entropy /usr/local/bin
 chmod +x /bin/entropy
 chmod +x /usr/local/bin/entropy
+cd ~/entropy
+pip3 install -r related.txt
 } &> /dev/null
 else
 cd ~
@@ -16,5 +18,7 @@ cp entropy /bin
 cp entropy /usr/local/bin
 chmod +x /bin/entropy
 chmod +x /usr/local/bin/entropy
+cd ~/entropy
+pip3 install -r related.txt
 } &> /dev/null
 fi
