3cff5f00a3d9d27f03fed6a3cadb267be2fdb4eb
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-05-20 20:19:33 +0300

    Update entropy.py

--- entropy.py
+++ entropy.py
@@ -475,7 +475,7 @@ def main():
     HEADER=fig.renderText('ENTROPY')
     VERSION='version:1.0'
     WRITER='http://entynetproject.simplesite.com/'
-    print(color.Headmagenta+HEADER.center(70)+color.Endlwhites)
+    print(color.Headmagenta+HEADER+color.Endlwhites)
     print(color.Headmagenta+VERSION.center(70)+color.Endlwhites)
     print(color.Headmagenta+WRITER.center(70)+color.Endlwhites)
     print(" ")
