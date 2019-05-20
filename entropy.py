65c4a6c0fd8cc43c75ca484c050c6e169fea8c5e
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-05-20 20:27:37 +0300

    Update entropy.py

--- entropy.py
+++ entropy.py
@@ -472,7 +472,7 @@ def crack(tmp_ip_list):
 def main():
     subprocess.Popen("clear",shell=True)
     fig=pyfiglet.Figlet('slant')
-    HEADER=os.system("cat banner/banner.py")
+    HEADER=fig.renderText('         ENTROPY')
     VERSION='version:1.0'
     WRITER='http://entynetproject.simplesite.com/'
     print(color.Headmagenta+HEADER+color.Endlwhites)
