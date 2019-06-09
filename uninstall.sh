69bc6809b43f1deb70a7d009314f372bdacf5473
Entynetproject <43011806+entynetproject@users.noreply.github.com>
2019-06-09 21:02:24 +0300

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -22,6 +22,22 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+CE="\033[0m"
+RS="\033[1;31m"
+YS="\033[1;33m"
+
+WHO="$( whoami )"
+
+if [[ "$WHO" != "root" ]]
+then
+sleep 1
+echo -e "$RS"run it as"$CE" "$YS"root"$CE"
+sleep 1
+echo -e "$RS"or use"$CE" "$YS"sudo"$CE"
+sleep 1
+exit
+fi
+
 {
 cd
 rm -r entropy
